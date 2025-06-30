cef3b9fff6dd69160fbb1eeb1f93739516b72c75
Mural Tnsae <tnsaealemayehu@gmail.com>
2025-06-30 08:35:40 +0000

    when i am trying to deploy it it gave me these errors     at i (/vercel/

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import { LoginForm } from "@/components/auth/LoginForm";
 import { PageWrapper } from "@/components/ui/PageWrapper";
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
   return (
     <PageWrapper className="flex min-h-[calc(100vh-theme(spacing.32))] flex-col items-center justify-center">
